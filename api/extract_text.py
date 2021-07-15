9809d45c98dd9d8885b868f6d5884382f8572b86
Krzysztof Ograbek <krzysztof.ograbek@gmail.com>
2021-07-15 08:19:32 +0200

    cleaning, small css changes

--- api/extract_text.py
+++ api/extract_text.py
@@ -1,69 +1,63 @@
 import spacy
 from spacy.matcher import Matcher
-from helpers import get_lines
-
 
 nlp = spacy.load('en_core_web_sm')
 
 
-def get_info():
-    lines = get_lines('../data/transcripts.txt')
-    txt = " ".join(lines)
-    doc = nlp(txt[:100])
-
-    infos = list()
-    for token in doc:
-        info = {
-            "text": token.text,
-            "pos": token.pos_,
-            "dep": token.dep_
-        }
-        infos.append(info)
-
-    print("getting infos")
-
-    return infos
-
 def get_sentences(lines):
-    # lines = get_lines('../data/transcripts.txt')
+    """" Returns sentences containing given keywords
+    
+    Parameters
+    ----------
+    lines : list(str)
+        text from transcripts as a list of strings
+    Returns
+    -------
+    sents : list(str)
+        a list sentences containing keywords and capitalized words in it
+    """
+
     txt = " ".join(lines)
-    # print("txt len: ", len(txt))
+    # convert text to Doc class from spaCy
     doc = nlp(txt)
 
     matcher = Matcher(nlp.vocab)
     sent_matcher = Matcher(nlp.vocab)
-    # Add match ID "HelloWorld" with no callback and one pattern
-    # pattern = [{"LOWER": "check"}, {"LOWER": "out"}]
-    book_pattern = [{"LEMMA": "book"}]
-    podcast_pattern = [{"LEMMA": "podcast"}]
-    website_pattern = [{"LEMMA": "website"}]
-    matcher.add("book", [book_pattern])
-    matcher.add("podcast", [podcast_pattern])
-    # matcher.add("course", [course_pattern])
-    matcher.add("website", [website_pattern])
+
+    # pattern to find keywords
+    # NOTE: We use `lemma` in order to also find plural forms
+    keyword_pattern = [{"LEMMA":  {"IN": ["book", "podcast", "website"]}}]
+    
+    matcher.add("book", [keyword_pattern])
+    # return all the tokens containing defined keywords
     matches = matcher(doc)
 
-    person_pattern = [{"ENT_TYPE": "PERSON"}]
-    person_pattern = [
-        [{"IS_TITLE": True, "IS_SENT_START": False, "LENGTH": {">=": 3}}],
+    # pattern for capitalized words
+    capitalized = [
+        # find words that start with a capital letter, exclude sentence beginnings
+        [{"IS_TITLE": True, "IS_SENT_START": False, "LENGTH": {">=": 2}}],
+        # find all PERSON Named Entities
         [{"ENT_TYPE": "PERSON"}]]
-    sent_matcher.add("person", person_pattern)
 
+    sent_matcher.add("person", capitalized)
+
+    # initialize a list to store sentences
     sents = list()
-    sent_ids = []
+    sent_ids = list()
     for match_id, start, end in matches:
         token = doc[start]
+        # get the sentence for the found word
         sent = token.sent
-
-        sent_doc = nlp(sent.text)
-        sent_matches = sent_matcher(sent_doc)
-        # print("sent matches", len(sent_matches))
-        if (sent.start not in sent_ids) and (len(sent_matches) > 0):
-            sents.append(sent.text)
-            sent_ids.append(sent.start)
-            # print([sent_doc[st] for _, st, _ in sent_matches])
+        if (sent.start not in sent_ids):        # check if already in sentences
+            # convert the sentence to Doc
+            sent_doc = nlp(sent.text)
+            # find Capitalized words in a sentence
+            sent_matches = sent_matcher(sent_doc)
+            if (len(sent_matches) > 0):         # if words found
+                # add to sentences we return
+                sents.append(sent.text)
+                sent_ids.append(sent.start)
 
     print("Found ", len(sents))
-    # print(sents)
 
     return sents
